373b335fb2e6d8b134081b31603bd1448ff99f32
GCidon <38065027+GCidon@users.noreply.github.com>
2019-05-26 11:19:19 +0200

    Partida terminada

--- DronProyect/DronProyect/Partida.xaml.cs
+++ DronProyect/DronProyect/Partida.xaml.cs
@@ -1,17 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
+using Windows.UI.Core;
+using Windows.UI.Xaml.Media.Imaging;
 
 // La plantilla de elemento Página en blanco está documentada en https://go.microsoft.com/fwlink/?LinkId=234238
 
@@ -22,14 +14,41 @@ namespace DronProyect
     /// </summary>
     public sealed partial class Partida : Page
     {
+
+        bool aux = true;
+
         public Partida()
         {
+            Window.Current.CoreWindow.KeyDown += Grid_KeyDown;
             this.InitializeComponent();
         }
 
         private void backtoMain(object sender, RoutedEventArgs e)
         {
             this.Frame.Navigate(typeof(MainPage));
+        }
+
+        private void Grid_KeyDown(CoreWindow sender, KeyEventArgs args)
+        {
+            switch (args.VirtualKey)
+            {
+                case Windows.System.VirtualKey.Escape:
+                    this.Frame.Navigate(typeof(MainPage));
+                    break;
+                case Windows.System.VirtualKey.Up:
+                    string s = "";
+                    if (aux)
+                    {
+                        s = Directory.GetCurrentDirectory() + "\\Assets\\street2.jpg";
+                        aux = !aux;
+                    } else
+                    {
+                        s = Directory.GetCurrentDirectory() + "\\Assets\\street.jpg";
+                        aux = !aux;
+                    }
+                    fondo.Source = new BitmapImage(new Uri(s));
+                    break;
+            }
         }
     }
 }
